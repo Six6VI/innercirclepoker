3155a575c32bc92958a9934b54a625c880cbf9a7
Six6VI <90937865+Six6VI@users.noreply.github.com>
2024-03-10 19:08:56 -0400

    Add files via upload

--- signin.js
+++ signin.js
@@ -1,11 +1,26 @@
 // Function to display the logged-in user's name in the navbar
+// Function to display the logged-in user's name in the navbar and redirect non-signed-on users
 function displayLoggedInUserName() {
     var loggedInUserName = sessionStorage.getItem('loggedInUserName');
     if (loggedInUserName) {
         var userDisplayNameElement = document.getElementById('userDisplayName');
         userDisplayNameElement.textContent = loggedInUserName;
+    } else {
+        // If the user is not logged in, redirect to the login page
+        window.location.href = 'index.html'; // Replace 'login.html' with the actual login page URL
     }
 }
 
 // Call the function to display the user's name when the DOM content is loaded
 document.addEventListener('DOMContentLoaded', displayLoggedInUserName);
+
+//function displayLoggedInUserName() {
+//    var loggedInUserName = sessionStorage.getItem('loggedInUserName');
+//    if (loggedInUserName) {
+//        var userDisplayNameElement = document.getElementById('userDisplayName');
+//        userDisplayNameElement.textContent = loggedInUserName;
+//    }
+//}
+//
+//// Call the function to display the user's name when the DOM content is loaded
+//document.addEventListener('DOMContentLoaded', displayLoggedInUserName);
